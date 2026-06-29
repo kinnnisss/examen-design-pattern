f6b26cc0d6968206f150d0a005fea762223eebc3
liivyo <liivyosn@gmail.com>
2026-06-29 09:13:27 +0000

    chore: initialiser les deux projets Spring Boot

---
+++ badwallet-api/src/test/java/edu/ism/badwallet_api/BadwalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ism.badwallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadwalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
